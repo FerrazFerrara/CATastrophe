3b5c1e52c3497835b5707f16ee619c06dee5c782
Gabriel Ferraz Fontes <brielff@hotmail.com>
2021-01-21 14:32:59 -0300

    Project created

---
+++ CATastrophe/CATastrophe/CATastropheApp.swift
@@ -0,0 +1,20 @@
+//
+//  CATastropheApp.swift
+//  CATastrophe
+//
+//  Created by Ferraz on 21/01/21.
+//
+
+import SwiftUI
+
+@main
+struct CATastropheApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
